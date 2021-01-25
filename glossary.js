2b9bffb8883f0bf94e44041263d55b0a00677692
Adam Newbold <adam@neatnik.net>
2021-01-25 10:42:10 -0500

    Update glossary.js

--- glossary.js
+++ glossary.js
@@ -3,8 +3,8 @@ for(var i = 0, len = elements.length; i < len; i++) {
 	elements [i].addEventListener('click', function() {
 		if(!this.dataset.clicked || this.dataset.clicked == 'false') {
 			this.dataset.clicked = 'true';
-			this.setAttribute('style', 'font-weight: bold; background: #333; color: #fff; padding: 0.2em 0.5em; border-top-left-radius: 0.3em; border-bottom-left-radius: 0.3em;');
-			this.insertAdjacentHTML('afterend', '<span style="background: #666; color: #fff; padding: 0.2em 0.5em; border-top-right-radius: 0.3em; border-bottom-right-radius: 0.3em;"><em>'+this.dataset.part+'</em>. '+this.dataset.definition+'</span>');
+			this.setAttribute('style', 'font-weight: bold; background: var(--background-tertiary); color: var(--foreground); padding: 0.2em 0.5em; border-top-left-radius: 0.3em; border-bottom-left-radius: 0.3em;');
+			this.insertAdjacentHTML('afterend', '<span style="background: var(--background-secondary); color: var(--foreground); padding: 0.2em 0.5em; border-top-right-radius: 0.3em; border-bottom-right-radius: 0.3em;"><em>'+this.dataset.part+'</em>. '+this.dataset.definition+'</span>');
 		}
 		else {
 			this.dataset.clicked = 'false';
